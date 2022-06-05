de0b4b783c16b5b1090c8c63a38e2e98c84015f5
Addison <addisonpomiri@gmail.com>
2022-06-05 18:12:27 +1200

    added the reply feature to our bot

--- NEO 1PG/src/bot.js
+++ NEO 1PG/src/bot.js
@@ -1,4 +1,23 @@
+import { config } from 'dotenv';
+config({ path: '.env' })
+
 import { Client, Intents } from 'discord.js'; 
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 bot.on('ready', () => console.log('ready'));
-bot.login('OTM0MzM2MTQyMDUwOTkyMjE4.YeumVw.t9hkIEgfFl5FLu0KUs4_S895gZ0');
+bot.on('message', async (whatever_the_user_type_will_be_stored_here) => {
+    //if(whatever_the_user_type_will_be_stored_here.author.bot) return;
+
+    //await whatever_the_user_type_will_be_stored_here.reply('blub blub');
+
+    //this is so that my bot will reply specifically
+    if(whatever_the_user_type_will_be_stored_here.content.startsWith("add")) {
+        whatever_the_user_type_will_be_stored_here.reply('blub blub');
+    }
+    //this is so that my bot will reply me in DM
+    if (whatever_the_user_type_will_be_stored_here.content.startsWith("adm")){
+        whatever_the_user_type_will_be_stored_here.member.send('blub blub! blub blub blub.');
+    }
+});     
+
+bot.login(process.env.BOT_TOKEN);
+
